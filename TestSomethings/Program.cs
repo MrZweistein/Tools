3d7a6427cdb34e76f1da5e5a0ce7ba1923c137a8
Roger S <roger.spiess@web.de>
2019-12-10 23:15:50 +0100

    Cleanup

--- TestSomethings/Program.cs
+++ TestSomethings/Program.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using IniParser.Model;
+using IniParser.Parser;
 
 namespace TestSomethings
 {
@@ -27,12 +29,32 @@ namespace TestSomethings
             //Console.CancelKeyPress += Console_CancelKeyPress;
             //Investigating();
             //Processing();
-            Data data = new Data() { a = "haha", b = 2 };
-            Thread thread = new Thread(Doing);
-            thread.Start(data);
-            Thread.Sleep(2000);
-            thread.Abort();
+            //Data data = new Data() { a = "haha", b = 2 };
+            //Thread thread = new Thread(Doing);
+            //thread.Start(data);
+            //Thread.Sleep(2000);
+            //thread.Abort();
+            TestIni();
             Console.ReadKey();
+        }
+
+        static private void TestIni()
+        {
+            string arguments = "";
+            arguments += $@"--preset ""Fast 1080p30"" ";
+            arguments += $@"--markers ";
+            arguments += $@"--align-av ";
+            arguments += $@"--decomb ";
+            arguments += $@"--all-subtitles ";
+            
+            Console.WriteLine(arguments);
+            IniDataParser Parser = new IniDataParser();
+            IniData Data = Parser.Parse("");
+            Data["Arguments"]["Line1"] = arguments;
+            Data["Arguments"]["Line2"] = "xxx";
+            Console.WriteLine(new string('+', 30));
+            Console.WriteLine(Data.ToString());
+            Console.WriteLine(Data["Arguments"]["Line1"]);
         }
 
         private static void Doing(object d)
@@ -119,7 +141,7 @@ namespace TestSomethings
 
                 using (pr = new Process())
                 {
-                    pr.StartInfo.FileName = @"d:\data\Dokumente\Visual Studio 2017\Projects\Tools\EndlessSummer\bin\Release\EndlessSummer.exe";
+                    pr.StartInfo.FileName = @"d:\data\Entwicklung\Tools\EndlessSummer\bin\Release\EndlessSummer.exe";
                     pr.StartInfo.Arguments = @"";
                     pr.StartInfo.CreateNoWindow = true;
                     pr.StartInfo.UseShellExecute = false;
