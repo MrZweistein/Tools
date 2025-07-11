98ef83d42a2dbf56cb8506eb63efc1b431f47cb8
Roger Spiess <roger.spiess@web.de>
2025-07-11 22:16:05 +0200

    Removed resources not needed in the executables

--- iCloudMonitor/Properties/Resources.Designer.cs
+++ iCloudMonitor/Properties/Resources.Designer.cs
@@ -79,15 +79,5 @@ namespace iCloudMonitor.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap MySplashIcon {
-            get {
-                object obj = ResourceManager.GetObject("MySplashIcon", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
